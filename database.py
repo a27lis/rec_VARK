babab4b2ca55b4f55f9f0fe494e44ad3f0781bd3
a27lis <anastasiya.liseykina@gmail.com>
2025-05-11 11:41:19 +0400

    changes

--- database.py
+++ database.py
@@ -5,15 +5,16 @@ from pydantic_settings import BaseSettings
 from typing import Generator
 import os
 from dotenv import load_dotenv
+from config import DB_USER, DB_PASS
 
 # Загружаем переменные окружения
 load_dotenv()
 
 class Settings(BaseSettings):
-    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_HOST: str = os.getenv("DB_HOST", "0.0.0.0:3306")
     DB_DATABASE: str = os.getenv("DB_DATABASE", "engforyou")
-    DB_USER: str = os.getenv("DB_USER", "root")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "root")
+    DB_USER: str = os.getenv("DB_USER", DB_USER)
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", DB_PASS)
 
 settings = Settings()
 
